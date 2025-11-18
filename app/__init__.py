6a31835306aeb99d7f560b41ab1fcc3d416f1d12
Jorge Garcia <yorchwebs@gmail.com>
2025-11-18 13:28:12 -0600

    Delete simple comments

--- app/__init__.py
+++ app/__init__.py
@@ -17,22 +17,18 @@ def create_app():
     """
     app = Flask(__name__)
 
-    # Habilita CORS para todas las rutas (puedes limitar dominios si lo deseas)
     CORS(app, origins=["localhost:5000/contact"])
 
-    # Configura claves desde entorno
     app.config["MAILERLITE_API_KEY"] = config("MAILERLITE_API_KEY", default="")
     app.config["MAILERLITE_GROUP_ID"] = config(
         "MAILERLITE_GROUP_ID", default=""
-    )  # noqa E501
+    )
 
-    # Verifica que no falten las claves críticas
     if not app.config["MAILERLITE_API_KEY"]:
         app.logger.warning("MAILERLITE_API_KEY está vacío o no definido.")
     if not app.config["MAILERLITE_GROUP_ID"]:
         app.logger.warning("MAILERLITE_GROUP_ID está vacío o no definido.")
 
-    # Registra las rutas
     app.register_blueprint(main_bp)
 
     return app
